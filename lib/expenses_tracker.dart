d43345b6df128acbc833f26de6d376ab111e3d23
Kaio Plandel <kaioplandel@gmail.com>
2023-07-24 06:11:12 -0300

    Initial commit

---
+++ lib/expenses_tracker.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
+
+class Expense extends StatefulWidget {
+  const Expense({Key? key}) : super(key: key);
+
+  @override
+  State<Expense> createState() => _ExpenseState();
+}
+
+class _ExpenseState extends State<Expense> {
+  final List<Expenses> _resgisteredExpenses = [
+    Expenses(
+        title: "Flutter course",
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expenses(
+        title: "Cinema",
+        amount: 59.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expenses(
+        title: "Pizza",
+        amount: 39.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expenses(
+        title: "Gym",
+        amount: 89.99,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expenses(
+        title: "Kotlin course",
+        amount: 39.99,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Text("The chart"), Text("Expenses list")],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:expense_tracker/expenses_tracker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Expense(),
+  ));
+}
